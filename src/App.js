d3478d201579686c048d297825120be6eb00aec7
Opaleye Boluwatife <ezekielcoolb@gmail.com>
2022-12-31 04:01:58 -0800

    commiting my currency conversion app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,276 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+function MyApp() {
+
+  const [currency, setCurrency] = useState('BRL')
+  const [rate, setRate] = useState('')
+  const [eur, seteur] = useState('')
+  const [gbp, setgbp] = useState('')
+  const [jpy, setjpy] = useState('')
+  const [cad, setcad] = useState('')
+  const [ngn, setngn] = useState('')
+  const [aud, setaud] = useState('')
+  const [nzd, setnzd] = useState('')
+
+  const [newrate, setnewRate] = useState('')
+  const [neweur, setneweur] = useState('')
+  const [newgbp, setnewgbp] = useState('')
+  const [newjpy, setnewjpy] = useState('')
+  const [newcad, setnewcad] = useState('')
+  const [newngn, setnewngn] = useState('')
+  const [newaud, setnewaud] = useState('')
+  const [newnzd, setnewnzd] = useState('')
+
+  const [yesterday, setYesterday] =useState('')
+
+  const rateChange = parseFloat((rate - newrate)).toFixed(2)
+  const eurChange = parseFloat((neweur - eur)).toFixed(2)
+  const gbpChange = parseFloat((newgbp - gbp)).toFixed(2)
+  const jpyChange = parseFloat((newjpy - jpy)).toFixed(2)
+  const cadChange = parseFloat((newcad - cad)).toFixed(2)
+  const ngnChange = parseFloat((newngn - ngn)).toFixed(2)
+  const audChange = parseFloat((newaud - aud)).toFixed(2)
+  const nzdChange = parseFloat((newnzd - nzd)).toFixed(2)
+  const percent = "%"
+
+
+//   const [today, setToday] = useState('')
+//  const [changes, setChanges] = useState('')
+
+  const handleChange = (e) => {
+    setCurrency(e.target.value)
+  }
+
+ useEffect(() => {
+  let date = new Date()
+     date.setDate(date.getDate() -1)
+    let newdate = date.toISOString().split('T')[0]
+    setYesterday(newdate)
+    console.log(newdate);
+  return () => console.log('i unmount');
+ }, [])
+  
+
+
+  useEffect(() => {
+    const url = `https://api.getgeoapi.com/v2/currency/convert?api_key=8d1d6d598c52a4201f2f7ede34fa5869e5463c69&from=USD&to=${currency}&amount=1&format=json`
+    // const curl = `https://api.apilayer.com/fixer/fluctuation?apikey=CWTC7We6DAzTLKQTf3wS8A5FE5MNHTwW&start_date=${yesterday}&end_date=${today}&base=USD&symbols=${currency}`
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url)
+        const data = await res.json()
+       const result = data.rates[currency].rate
+         
+        
+        setRate(result)
+
+       
+      } catch (error) {
+        console.log("error", error);
+      }
+    }
+    fetchData()
+  }, [currency])
+
+
+  useEffect(() => {
+    const url = `https://api.getgeoapi.com/v2/currency/convert?api_key=8d1d6d598c52a4201f2f7ede34fa5869e5463c69&from=USD&to=EUR,GBP,CAD,AUD,JPY,NGN,NZD&amount=1&format=json`
+
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url)
+        const data = await res.json()
+        const eur =  data.rates.EUR.rate
+        seteur(eur)
+
+        const gbp = data.rates.GBP.rate
+        setgbp(gbp)
+
+        const jpy = data.rates.JPY.rate
+        setjpy(jpy)
+
+        const cad = data.rates.CAD.rate
+        setcad(cad)
+
+        const ngn = data.rates.NGN.rate
+        setngn(ngn)
+
+        const aud = data.rates.AUD.rate
+        setaud(aud)
+
+        const nzd = data.rates.NZD.rate
+        setnzd(nzd)
+
+      } catch (error) {
+        console.log("error", error);
+      }
+    }
+    fetchData()
+  }, [])
+
+useEffect(() => {
+  const url = `https://api.getgeoapi.com/v2/currency/convert?api_key=8d1d6d598c52a4201f2f7ede34fa5869e5463c69&updated_date=${yesterday}from=USD&to=EUR,GBP,CAD,AUD,JPY,NGN,NZD,${currency}&amount=1&format=json`
+
+  const fetchData = async () => {
+
+    try {
+      
+      const res = await fetch(url)
+      const data = await res.json()
+      const eur =  data.rates.EUR.rate
+        setneweur(eur)
+
+        const gbp = data.rates.GBP.rate
+        setnewgbp(gbp)
+
+        const jpy = data.rates.JPY.rate
+        setnewjpy(jpy)
+
+        const cad = data.rates.CAD.rate
+        setnewcad(cad)
+
+        const ngn = data.rates.NGN.rate
+        setnewngn(ngn)
+
+        const aud = data.rates.AUD.rate
+        setnewaud(aud)
+
+        const nzd = data.rates.NZD.rate
+        setnewnzd(nzd)
+
+        const result = data.rates[currency].rate 
+        setnewRate(result)
+
+
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
+  fetchData()
+}, [yesterday, currency])
+
+  return (
+    <div>
+      
+      <section>
+        <h2>Tife-Code Exchange Rates</h2> <br />
+        <table>
+          <tr >
+            <th>Currency</th>
+            <th>Amount</th>
+            <th>Change in 24hours</th>
+          </tr>
+          <tr className="header">
+            <th>US Dollar</th>
+            <th>1</th>
+            <th></th>
+          </tr>
+          <tr>
+            <td>Euro</td>
+            <td>{eur}</td>
+            <td style={{
+            color: eurChange > 0 ? 'green' : 'red'
+          }}>{eurChange}{percent}</td>
+          </tr>
+          <tr>
+            <td>British Pound</td>
+            <td>{gbp}</td>
+            <td style={{
+            color: gbpChange > 0 ? 'green' : 'red'
+          }}>{gbpChange}{percent}</td>
+          </tr>
+          <tr>
+            <td>Japanese Yen</td>
+            <td>{jpy}</td>
+            <td style={{
+            color: jpyChange > 0 ? 'green' : 'red'
+          }}>{jpyChange}{percent}</td>
+          </tr>
+          <tr>
+            <td>Canadian Dollar</td>
+            <td>{cad}</td>
+            <td style={{
+            color: cadChange > 0 ? 'green' : 'red'
+          }}>{cadChange}{percent}</td>
+          </tr>
+          <tr>
+            <td>Nigerian naira</td>
+            
+            <td>{ngn}</td>
+            <td style={{
+            color: ngnChange > 0 ? 'green' : 'red'
+          }}>{ngnChange}{percent}</td>
+          </tr>
+          <tr>
+            <td>Australian Dollar</td>
+            <td>{aud}</td>
+            <td style={{
+            color: audChange > 0 ? 'green' : 'red'
+          }}>{audChange}{percent}</td>
+          </tr>
+          <tr>
+            <td>New Zealand Dollar</td>
+            <td>{nzd}</td>
+            <td style={{
+            color: nzdChange > 0 ? 'green' : 'red'
+          }}> {nzdChange}{percent}</td>
+          
+          </tr>
+          <tr>
+            <td>{currency}</td>
+            <td>{rate}</td>
+            <td style={{
+            color: rateChange > 0 ? 'green' : 'red'
+          }}>{rateChange}{percent}</td>
+          </tr>
+        </table>
+
+     
+      </section>
+     
+      <select onChange = { (e) => handleChange(e)}>
+                <option disabled ="disabled" style={{color: 'blue'}}> Add Cureency</option>
+                <option value="BRL">BRL - Brazilian Real</option>
+                <option value="CHN">CNH - Chinese Yuan Offshore</option>
+                <option value="CNY">CNY - Chinese Yuan</option>
+                <option value="EGP">EGP - Egyptian Pound</option>
+                <option value="ETB">ETB - Ethiopian Birr</option>
+                <option value="GHS">GHS - Ghanaian Cedi</option>
+                <option value="IDR">IDR -Indonesian Rupiah </option>
+                <option value="INR">INR - Indian Rupee</option>
+                <option value="JMD">JMD - Jamaican Dollar</option>
+                <option value="KWD">KWD - Kuwaiti Dinar</option>
+                <option value="KPW">KPW - North Korean Won</option>
+                <option value="KRW">KRW - South Korean Won</option>
+                <option value="LRD">LRD - Liberian Dollar</option>
+                <option value="LBP">LBP - Lebanese Pound</option>
+                <option value="MXN">MXN - Mexican Peso</option>
+                <option value="PHP">PHP - Philippine Peso</option>
+                <option value="PKR">PKR - Pakistani Rupee</option>
+                <option value="QAR">QAR - Qatari Rial</option>
+                <option value="RSD">RSD - Serbian Dinar</option>
+                <option value="RUB">RUB - Russian Ruble</option>
+                <option value="SYP">SYP - Syrian Pound</option>
+                <option value="TWD">TWD - Taiwan New Dollar</option>
+                <option value="UAH">UAH - Ukrainian Hryvnia</option>
+                <option value="USD">USD - United States Dollar</option>
+                <option value="XCD">XCD - East Caribbean Dollar</option>
+                <option value="YER">YER - Yemeni Rial</option>
+                <option value="ZAR">ZAR - South African Rand</option>
+                <option value="ZMW">ZMW - Zambian Kwacha</option>
+      </select>
+      
+     
+    </div>
+  )
+}
+
+  
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyApp />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,9 +5,249 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+class GetFetch extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currency: 'AUD',
+      baseCurrency: 'AUD',
+      input: '',
+      amount: '',
+      output1: ``,
+      output2:``,
+      output3: ``,
+      output4: ``,
+      output5: ``,
+      rate: ''
+        }
+    this.handleChanged = this.handleChanged.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleChanges = this.handleChanges.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+  
+  handleChange(e) {
+    this.setState({
+      currency: e.target.value
+    })
+  }
+
+  handleChanged(e) {
+    this.setState({
+      baseCurrency: e.target.value
+    })
+  }
+  handleChanges(e) {
+    this.setState({
+      input: e.target.value
+    })
+  }
+  handleClick() {
+    
+      fetch(`https://api.getgeoapi.com/v2/currency/convert?api_key=8d1d6d598c52a4201f2f7ede34fa5869e5463c69&from=${this.state.baseCurrency}&to=${this.state.currency}&amount=${this.state.input}&format=json`)
+        .then(res => res.json())
+        .then (data => {
+            const client = data.rates[this.state.currency].rate
+    
+            const result =  this.state.input * client
+
+            this.setState({
+              amount: result,
+              output1: `${this.state.input} ${this.state.baseCurrency} =  `,
+              output2: `${this.state.currency}`,
+              output3: `1 ${this.state.baseCurrency} =`,
+              output4: `${this.state.currency}`,
+              output5: `You converted ${this.state.input} ${this.state.baseCurrency} to ${this.state.currency}`,
+              rate: client
+            })
+            
+            
+        })
+    
+        .catch(err => {
+            console.log(`error ${err}`);
+        })
+  }
+
+  render() {
+
+  
+
+    return (
+      <div >
+         <h1>Tife-Code Currency Converter</h1>
+        <section className='pageBody'>
+            <header>
+             
+              <h2>{this.state.output5}</h2>
+            </header>
+            <section className='getFetchBody'>
+
+              
+              <label>
+                Amount: <br />
+                  <input className='currency'  type = "text" onChange = {this.handleChanges}></input>
+              </label>
+            
+              <label>
+                From:
+                <select className='currency' name="" value = {this.state.baseCurrency} onChange = {this.handleChanged}>
+              
+                <option value="AUD">AUD - Australian Dollar</option>
+                <option value="BRL">BRL - Brazilian Real</option>
+                <option value="CAD">CAD - Canadian Dollar</option>
+                <option value="CNH">CNH - Chinese Yuan Offshore</option>
+                <option value="CNY">CNY - Chinese Yuan</option>
+                <option value="EGP">EGP - Egyptian Pound</option>
+                <option value="ETB">ETB - Ethiopian Birr</option>
+                <option value="EUR">EUR - Euro</option>
+                <option value="GBP">GBP - British Pound Sterling</option>
+                <option value="GHS">GHS - Ghanaian Cedi</option>
+                <option value="IDR">IDR -Indonesian Rupiah </option>
+                <option value="INR">INR - Indian Rupee</option>
+                <option value="JPY">JPY - Japanese Yen</option>
+                <option value="JMD">JMD - Jamaican Dollar</option>
+                <option value="KWD">KWD - Kuwaiti Dinar</option>
+                <option value="KPW">KPW - North Korean Won</option>
+                <option value="KRW">KRW - South Korean Won</option>
+                <option value="LRD">LRD - Liberian Dollar</option>
+                <option value="LBP">LBP - Lebanese Pound</option>
+                <option value="MXN">MXN - Mexican Peso</option>
+                <option value="NGN">NGN - Nigerian Naira</option>
+                <option value="NZD">NZD - New Zealand Dollar</option>
+                <option value="PHP">PHP - Philippine Peso</option>
+                <option value="PKR">PKR - Pakistani Rupee</option>
+                <option value="QAR">QAR - Qatari Rial</option>
+                <option value="RSD">RSD - Serbian Dinar</option>
+                <option value="RUB">RUB - Russian Ruble</option>
+                <option value="SYP">SYP - Syrian Pound</option>
+                <option value="TWD">TWD - Taiwan New Dollar</option>
+                <option value="UAH">UAH - Ukrainian Hryvnia</option>
+                <option value="USD">USD - United States Dollar</option>
+                <option value="XCD">XCD - East Caribbean Dollar</option>
+                <option value="YER">YER - Yemeni Rial</option>
+                <option value="ZAR">ZAR - South African Rand</option>
+                <option value="ZMW">ZMW - Zambian Kwacha</option>
+              </select>
+              </label>
+              
+            <label>
+              To:
+            <select className='currency' name="" value = {this.state.currency} onChange={this.handleChange}>
+              <option value="AUD">AUD - Australian Dollar</option>
+                <option value="BRL">BRL - Brazilian Real</option>
+                <option value="CAD">CAD - Canadian Dollar</option>
+                <option value="CNH">CNH - Chinese Yuan Offshore</option>
+                <option value="CNY">CNY - Chinese Yuan</option>
+                <option value="EGP">EGP - Egyptian Pound</option>
+                <option value="ETB">ETB - Ethiopian Birr</option>
+                <option value="EUR">EUR - Euro</option>
+                <option value="GBP">GBP - British Pound Sterling</option>
+                <option value="GHS">GHS - Ghanaian Cedi</option>
+                <option value="IDR">IDR -Indonesian Rupiah </option>
+                <option value="INR">INR - Indian Rupee</option>
+                <option value="JPY">JPY - Japanese Yen</option>
+                <option value="JMD">JMD - Jamaican Dollar</option>
+                <option value="KWD">KWD - Kuwaiti Dinar</option>
+                <option value="KPW">KPW - North Korean Won</option>
+                <option value="KRW">KRW - South Korean Won</option>
+                <option value="LRD">LRD - Liberian Dollar</option>
+                <option value="LBP">LBP - Lebanese Pound</option>
+                <option value="MXN">MXN - Mexican Peso</option>
+                <option value="NGN">NGN - Nigerian Naira</option>
+                <option value="NZD">NZD - New Zealand Dollar</option>
+                <option value="PHP">PHP - Philippine Peso</option>
+                <option value="PKR">PKR - Pakistani Rupee</option>
+                <option value="QAR">QAR - Qatari Rial</option>
+                <option value="RSD">RSD - Serbian Dinar</option>
+                <option value="RUB">RUB - Russian Ruble</option>
+                <option value="SYP">SYP - Syrian Pound</option>
+                <option value="TWD">TWD - Taiwan New Dollar</option>
+                <option value="UAH">UAH - Ukrainian Hryvnia</option>
+                <option value="USD">USD - United States Dollar</option>
+                <option value="XCD">XCD - East Caribbean Dollar</option>
+                <option value="YER">YER - Yemeni Rial</option>
+                <option value="ZAR">ZAR - South African Rand</option>
+                <option value="ZMW">ZMW - Zambian Kwacha</option>
+              </select>
+            </label>
+              <br />
+              
+              <section className='result'>
+                <p className='resultReal'><span  className='paraOne'>{this.state.output1}</span> <br /><span className='paraTwo'> {this.state.amount} <span className='paraColor'>{this.state.output2}</span></span> </p>
+                <p className='paraThree'> {this.state.output3}  {this.state.rate} {this.state.output4}</p>
+              </section>
+              <section className='button'>
+                <p className='info'>We use the mid-market rate for our Converter. This is for informational purposes only</p>
+                <button className='btn' onClick = {this.handleClick}>Convert</button>
+              </section>
+              
+              
+            </section>
+            < App />
+            
+            
+        </section>
+        
+        <section className='footer'>
+              <h2>Supported Currencies</h2>
+            <section className='footerSec'>
+              <section>
+                 <p>AUD - Australian Dollar</p>
+                <p>BRL - Brazilian Real</p>
+                <p>CAD - Canadian Dollar</p>
+                <p>CNH - Chinese Yuan Offshore</p>
+                <p>CNY - Chinese Yuan</p>
+                <p>EGP - Egyptian Pound</p>
+                <p>ETB - Ethiopian Birr</p>
+                <p>EUR - Euro</p>
+                <p>GBP - British Pound Sterling</p>
+              </section>
+              <section>
+              <p>GHS - Ghanaian Cedi</p>
+                <p>IDR -Indonesian Rupiah </p>
+                <p>INR - Indian Rupee</p>
+                <p>JPY - Japanese Yen</p>
+                <p>JMD - Jamaican Dollar</p>
+                <p>KWD - Kuwaiti Dinar</p>
+                <p>KPW - North Korean Won</p>
+                <p>KRW - South Korean Won</p>
+                <p>LRD - Liberian Dollar</p>
+              </section>
+              <section>
+              <p>LBP - Lebanese Pound</p>
+                <p>MXN - Mexican Peso</p>
+                <p>NGN - Nigerian Naira</p>
+                <p>NZD - New Zealand Dollar</p>
+                <p>PHP - Philippine Peso</p>
+                <p>PKR - Pakistani Rupee</p>
+                <p>QAR - Qatari Rial</p>
+                <p>RSD - Serbian Dinar</p>
+                <p>RUB - Russian Ruble</p>
+              </section>
+              <section>
+              <p>SYP - Syrian Pound</p>
+                <p>TWD - Taiwan New Dollar</p>
+                <p>UAH - Ukrainian Hryvnia</p>
+                <p>USD - United States Dollar</p>
+                <p>XCD - East Caribbean Dollar</p>
+                <p>YER - Yemeni Rial</p>
+                <p>ZAR - South African Rand</p>
+                <p>ZMW - Zambian Kwacha</p>
+              </section>
+            </section>
+            </section>
+      </div>
+    )
+  }
+}
 root.render(
   <React.StrictMode>
-    <App />
+    <GetFetch />
+    
   </React.StrictMode>
 );
 
